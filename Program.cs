4294b4b3391fc3643088951954f5c4765ec66b4a
mustafayilmazz0 <mustafayilmazz91@gmail.com>
2022-11-11 17:47:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// out parametreler
+string sayi="999";
+int outSayi;
+
+bool sonuc = int.TryParse(sayi, out outSayi);
+if(sonuc)
+{
+    Console.WriteLine("Başarılı");
+    Console.WriteLine(outSayi);
+}
+else
+{
+        Console.WriteLine("Başarısız");
+
+}
+
+ Metotlar instance = new Metotlar();
+ instance.Topla(4,5,out int toplamSonucu);
+  Console.WriteLine("toplamSonucu");
+
+
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam)
+{
+    toplam = a+b;
+}
+}
